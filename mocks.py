d5c584d666a163a208e6bd23cbc277c0304efa85
zhaoyanz405 <zhaoyanz405@gmail.com>
2023-03-11 14:24:09 +0800

    fix testcases for mock

--- mocks.py
+++ mocks.py
@@ -1,20 +1,21 @@
-def string(max_length):
+def string(max_length=10):
     """only for testing
 
     Args:
-        channel (_type_): _description_
+        max_length (int): the length of the string to generated
 
     """
+
     def deco(func):
         def wrap(*args, **kwargs):
             import random
-            
+
             res = ""
             for i in range(random.randint(0, max_length)):
-                res += random.choice('abcdefghijklmnopqrstuvwxyz ') # include the space
+                res += random.choice('abcdefghijklmnopqrstuvwxyz ')  # include the space
 
             return res
-        
+
         return wrap
-    
+
     return deco
